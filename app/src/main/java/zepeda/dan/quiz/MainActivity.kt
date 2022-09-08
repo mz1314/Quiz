43bc7882bcc52b06204c69a44fe9be5c559619c6
Misael <zepedamisael231@gmail.com>
2022-09-08 10:24:14 -0500

    clase del 8

--- app/src/main/java/zepeda/dan/quiz/MainActivity.kt
+++ app/src/main/java/zepeda/dan/quiz/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
     private var Indice=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate called")
+        Log.d(TAG, "onCreate Called")
         //setContentView(R.layout.activity_main)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
